bfaf98332774772559c1df99e3aa8f767c7cfb9f
hafiane-hamza <hamza.hafiane1@gmail>
2018-10-11 23:51:07 +0200

    formGroup login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgForm, FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({templateUrl: 'login.component.html'})
 
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
+  form: FormGroup;
   loading = false;
   submitted = false;
   returnUrl: string;
 
   constructor(
     public formBuilder: FormBuilder,
+    public fb: FormBuilder,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {
+    this.form = fb.group({
+      email: ['', [Validators.required, Validators.minLength(3), Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+    });
+  }
 
   ngOnInit() { }
+  login() {
+    this.router.navigate(['/condition']);
+          //this.router.navigate([this.returnUrl]);
+          //setTimeout(() => {
+           // location.reload();
+          //}, 1000)
+
+
+  }
 
 }
